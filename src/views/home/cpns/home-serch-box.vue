709e2206e2c20e2198829fcd95a2e91036375f88
Aiden027 <aiden02@qq.com>
2022-10-26 14:22:05 +0800

    feat

---
+++ src/views/home/cpns/home-serch-box.vue
@@ -0,0 +1,50 @@
+<script setup>
+const positionClick = () => {
+  navigator.geolocation.getCurrentPosition((res) => {
+    console.log(res);
+  }, (err) => {
+    console.log(err);
+  }, {
+    timeout: 5000
+  })
+}
+</script>
+
+<template>
+  <div class="home-serch-box">
+    <div class="location">
+      <div class="city">广州</div>
+      <div class="position" @click="positionClick">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+
+  .city {
+    flex: 1;
+  }
+
+  .position {
+    width: 74px;
+
+    .text {
+      font-size: 12px;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 18px;
+      height: 18px;
+    }
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,22 @@
 <script setup>
 import homeNavBarVue from './cpns/home-nav-bar.vue';
+import homeSerchBoxVue from './cpns/home-serch-box.vue';
 </script>
 
 <template>
   <div class="home">
     <homeNavBarVue />
+    <div class="banner">
+      <img src="@/assets/img/home/banner.webp" alt="">
+    </div>
+    <homeSerchBoxVue></homeSerchBoxVue>
   </div>
 </template>
 
 <style lang="less" scoped>
-
+.banner {
+  img {
+    width: 100%;
+  }
+}
 </style>
